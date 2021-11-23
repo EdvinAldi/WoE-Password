b79d8798913e5df788cd163b66f6bbc142f128b1
EdvinAldi <83842201+EdvinAldi@users.noreply.github.com>
2021-11-23 18:02:58 +0100

    Epic javascript
    lets go

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,78 @@
-document.getElementById("CharacterAmount")
+function betterMathRandom() {
+  const charArray = new Uint8Array(1);
+  crypto.getRandomValues(charArray);
+  const betterRandom = charArray[0] / 255;
+  return betterRandom;
+}
+
+const PasswordLength = document.getElementById("NumberOfCharacters");
+
+PasswordLength.addEventListener("input", (Event => {
+  const output = document.getElementById("output");
+  if (Event.target.value > 32) {
+    output.innerText = "Måste vara ett mindre än 33"
+  }
+  else {
+    output.innerText = makeNumberPassword(Event.target.value)
+  }
+}));
+
+
+function randomUpperCase() {
+  result = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  return result;
+}
+
+function randomLowerCase() {
+  result = "";
+  const characters = "abcdefghijklmnopqrstuvwxyz";
+  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  return result;
+}
+
+function specialCharacter () {
+  result = "";
+  const characters = "!?£\"§∞@"
+  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  return result;
+}
+
+function numberCharacter () {
+  result = "";
+  const characters = "0123456789"
+  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  return result;
+}
 
 
 
-const SpecCharacterCheckbox = document.getElementById("SpecialCharacter")
+function makeTheBestPassword(length) {
+  let result = '';
+  const characters = "";
+  const charactersLength = characters.length;
+  const randomCharacterArray = [randomUpperCase,randomLowerCase,specialCharacter,numberCharacter];
 
+  for (let i = 0; i < length; i++) {
+    let test = getRandomInt();
+    result += randomCharacterArray[test]();
 
+  }
+  return result;
+}
 
-var CapitalLettersCheckbox = document.getElementById("CapitalLetters")
+function getRandomInt() {
+  return Math.floor(betterMathRandom() * 4);
+}
 
-
-
-var LowerCaseCheckbox = document.getElementById("LowerCaseLetters")
-
-console.log(SpecCharacterCheckbox.c)
+function makeNumberPassword (length) {
+  let result = '';
+  const characters = '0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(betterMathRandom() *
+      charactersLength));
+  }
+  return result;
+}
