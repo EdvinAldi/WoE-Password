d2c7037b11a33c155a5f2f9277b0d645eca0e78c
EdvinAldi <83842201+EdvinAldi@users.noreply.github.com>
2021-11-25 14:38:05 +0100

    add comments and refactoring

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,8 @@
-function betterMathRandom() {
-  const charArray = new Uint32Array(1);
-  crypto.getRandomValues(charArray);
-  const betterRandom = charArray[0] / 4294967295;
-  return betterRandom;
-}
+import { betterMathRandom } from "./betterMathRandom.js";
 
 const PasswordLength = document.getElementById("NumberOfCharacters");
 
+//Event listener listening to number input from user
 PasswordLength.addEventListener("input", (Event => {
   const output = document.getElementById("output");
   if (Event.target.value > 32) {
@@ -17,88 +13,77 @@ PasswordLength.addEventListener("input", (Event => {
   }
 }));
 
-function getNumberOfCharacters () {
+//Gets number of characters from input number field
+function getNumberOfCharacters() {
   const numberOfCharacters = document.getElementById("NumberOfCharacters");
-  console.dir(numberOfCharacters.value);
   return numberOfCharacters.value;
 }
 
 //Returns one random UpperCase letter
 function randomUpperCase() {
-  result = "";
+  let result = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  result += characters.charAt(Math.floor(betterMathRandom() * characters.length))
   return result;
 }
 
 //Returns one random LowerCase letter
 function randomLowerCase() {
-  result = "";
+  let result = "";
   const characters = "abcdefghijklmnopqrstuvwxyz";
-  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  result += characters.charAt(Math.floor(betterMathRandom() * characters.length))
   return result;
 }
 
 //Returns one random special character
-function specialCharacter () {
-  result = "";
+function specialCharacter() {
+  let result = "";
   const characters = "!?£\"§∞@"
-  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  result += characters.charAt(Math.floor(betterMathRandom() * characters.length))
   return result;
 }
 
 //Returns one random number 
-function numberCharacter () {
-  result = "";
+function numberCharacter() {
+  let result = "";
   const characters = "0123456789"
-  result += characters.charAt(Math.floor(betterMathRandom()* characters.length))
+  result += characters.charAt(Math.floor(betterMathRandom() * characters.length))
   return result;
 }
 
 
-
+//Uses arrayBuilder method to build array with functions.
+//Takes user input and randomizes characters depending on functions in array
 function makeTheBestPassword(length) {
   let result = '';
-   const randomCharacterArray = [randomUpperCase,randomLowerCase,specialCharacter,numberCharacter];
-  const KukArray = arrayBuilder();
-  console.dir(KukArray);
-
-  
-  
+  const functionArray = arrayBuilder();
 
   for (let i = 0; i < length; i++) {
-    // result += randomCharacterArray[test]();
     let test = getRandomInt();
-    result += KukArray[test]();
+    result += functionArray[test]();
   }
-  console.dir(result);
   return result;
 }
 
+//Returns random int between 0 and length of array.
 function getRandomInt() {
   return Math.floor(betterMathRandom() * arrayBuilder().length);
 }
 
-function makeNumberPassword (length) {
-  let result = '';
-  const characters = '0123456789';
-  const charactersLength = characters.length;
-  for (let i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(betterMathRandom() *
-      charactersLength));
-  }
-  return result;
-}
 
 const GeneratePassword = document.getElementById("GeneratePassword");
-GeneratePassword.addEventListener("click", function() {
+
+//Eventlistener on "Generera" button and sets password output to makeTheBestPassword function that takes getNumberOfCharacters as params.
+GeneratePassword.addEventListener("click", function () {
   const output = document.getElementById("output");
   output.innerText = makeTheBestPassword(getNumberOfCharacters());
 });
 
-function arrayBuilder () {
+//Builds an array with functions depending on their boolean value from checkboxes.
+//Always includes numberCharacters as that is baseline for passwords.
+function arrayBuilder() {
 
-  let randomCharacterArray =  [numberCharacter];
+  let randomCharacterArray = [numberCharacter];
 
   if (document.getElementById("SpecialCharacter").checked === true) {
     randomCharacterArray.push(specialCharacter);
@@ -106,7 +91,7 @@ function arrayBuilder () {
   if (document.getElementById("CapitalLetters").checked === true) {
     randomCharacterArray.push(randomUpperCase);
   }
-  if(document.getElementById("LowerCaseLetters").checked === true) {
+  if (document.getElementById("LowerCaseLetters").checked === true) {
     randomCharacterArray.push(randomLowerCase);
   }
   return randomCharacterArray;
